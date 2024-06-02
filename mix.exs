e927300b2a13d66eee10da20baad3b6407244103
Chris Nguyen <chris.dungnguyen@gmail.com>
2024-06-02 09:42:24 +0700

    feat: update config

--- mix.exs
+++ mix.exs
@@ -1,12 +1,17 @@
 defmodule Genie.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/ChrisNg02655332/genie.git"
+  @version "0.0.1"
+
   def project do
     [
       app: :genie,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -23,6 +28,21 @@ defmodule Genie.MixProject do
     [
       {:req, "~> 0.4.0"},
       {:ecto, "~> 3.0"}
+    ]
+  end
+
+  defp description() do
+    """
+    This library wraps API to integrate with external AI such as openai 
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Chris Nguyen"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url},
+      files: ~w(mix.exs lib README.md LICENSE.md)
     ]
   end
 end
